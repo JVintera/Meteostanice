9ccb5c1400e9b692c1b468d41e9c5bb16fa61e8f
Jiri Vintera <jvintera@spselitdobruska.cz>
2024-06-02 23:49:57 +0200

    Smazání nedodělaných verzí, smazání Readme.md, založení projektu Meteo_mini_v2 - venkovní meteostanice

--- ESP32 Meteo mini v3.2/Meteo_mini/config.h
+++ ESP32 Meteo mini v3.2/Meteo_mini/config.h
@@ -1,3 +1,6 @@
+// json soubor na SD kartě
+// wifi_credentials.json
+
 // vypln tvou domenu cidla, kterou sis zaregistroval na tmep.cz
 String serverName = "http://TVADOMENA.tmep.cz/index.php?";
 // vypln tve GUID cidla
